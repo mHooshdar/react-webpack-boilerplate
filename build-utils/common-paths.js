02d692b852ad4de27dfe340fc007861fc8e56f0a
Mohammad Hooshdar <m.hooshdar@alibaba.ir>
2020-09-12 23:32:23 +0430

    chore: update git ignore

--- build-utils/common-paths.js
+++ build-utils/common-paths.js
@@ -3,6 +3,6 @@ const PROJECT_ROOT = path.resolve(__dirname, '../');
 
 module.exports = {
   projectRoot: PROJECT_ROOT,
-  outputPath: path.join(PROJECT_ROOT, 'dist'),
+  outputPath: path.join(PROJECT_ROOT, 'build'),
   appEntry: path.join(PROJECT_ROOT, 'src'),
 };
